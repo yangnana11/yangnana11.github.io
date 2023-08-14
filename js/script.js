565c4b8c1d1588220f586c1e65ab88eec57990c5
yangnana11 <yphan1988@gmail.com>
2023-08-14 15:04:28 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,242 @@
+const projects = [
+    {
+        "name": "superhairpieces",
+        "key": "superhairpieces",
+        "skills": "HTML, CSS, JS, JQuery, PHP, NodeJS, AWS, MySQL, RestAPI, Chrome Extension, BigCommerce, HandleBars, GraphQL",
+        "url": "www.superhairpieces.com",
+        "thumbnails": {
+            "main": {
+                "link": "data/projects/superhairpieces/thumbnail-mobile.jpg",
+                "type": "image/jpeg"
+            },
+            "alt": {
+                "link": "data/projects/superhairpieces/thumbnail-mobile.webp",
+                "type": "image/webp"
+            },
+            "pc-main": {
+                "link": "data/projects/superhairpieces/thumbnail-pc.jpg",
+                "type": "image/jpeg"
+            },
+            "pc-alt": {
+                "link": "data/projects/superhairpieces/thumbnail-pc.webp",
+                "type": "image/webp"
+            }
+        }
+    },{
+        "name": "privy",
+        "key": "privy",
+        "skills": "DJango, PHP, HTML, CSS, JQuery, RestAPI, Liquid, Shopify",
+        "url": "privyusa.com",
+        "thumbnails": {
+            "main": {
+                "link": "data/projects/privyusa/thumbnail-mobile.jpg",
+                "type": "image/jpeg"
+            },
+            "alt": {
+                "link": "data/projects/privyusa/thumbnail-mobile.webp",
+                "type": "image/webp"
+            },
+            "pc-main": {
+                "link": "data/projects/privyusa/thumbnail-pc.jpg",
+                "type": "image/jpeg"
+            },
+            "pc-alt": {
+                "link": "data/projects/privyusa/thumbnail-pc.webp",
+                "type": "image/webp"
+            }
+        }
+    },{
+        "name": "chrome extension",
+        "key": "chrome",
+        "skills": "JS, NodeJS, Chrome API, Chrome Extension",
+        "thumbnails": {
+            "main": {
+                "link": "data/projects/superhairpieces-extension/thumbnail-mobile.jpg",
+                "type": "image/jpeg"
+            },
+            "alt": {
+                "link": "data/projects/superhairpieces-extension/thumbnail-mobile.webp",
+                "type": "image/webp"
+            },
+            "pc-main": {
+                "link": "data/projects/superhairpieces-extension/thumbnail-pc.jpg",
+                "type": "image/jpeg"
+            },
+            "pc-alt": {
+                "link": "data/projects/superhairpieces-extension/thumbnail-pc.webp",
+                "type": "image/webp"
+            }
+        }
+    },{
+        "name": "nowon",
+        "key": "nowon",
+        "skills": "HTML, CSS, JS, JQuery, PHP, RestAPI",
+        "url": "nowonpay.kr",
+        "thumbnails": {
+            "main": {
+                "link": "data/projects/nowon/thumbnail-mobile.jpg",
+                "type": "image/jpeg"
+            },
+            "alt": {
+                "link": "data/projects/nowon/thumbnail-mobile.webp",
+                "type": "image/webp"
+            },
+            "pc-main": {
+                "link": "data/projects/nowon/thumbnail-pc.jpg",
+                "type": "image/jpeg"
+            },
+            "pc-alt": {
+                "link": "data/projects/nowon/thumbnail-pc.webp",
+                "type": "image/webp"
+            }
+        }
+    },{
+        "name": "next ico",
+        "key": "next-ico",
+        "skills": "React-Native",
+        "thumbnails": {
+            "main": {
+                "link": "data/projects/next-ico/thumbnail-mobile.jpg",
+                "type": "image/jpeg"
+            },
+            "alt": {
+                "link": "data/projects/next-ico/thumbnail-mobile.webp",
+                "type": "image/webp"
+            },
+            "pc-main": {
+                "link": "data/projects/next-ico/thumbnail-pc.jpg",
+                "type": "image/jpeg"
+            },
+            "pc-alt": {
+                "link": "data/projects/next-ico/thumbnail-pc.webp",
+                "type": "image/webp"
+            }
+        }
+    },{
+        "name": "hycon",
+        "key": "hycon",
+        "skills": "HTML, CSS, JS, JQuery",
+        "thumbnails": {
+            "main": {
+                "link": "data/projects/hycon/thumbnail-mobile.jpg",
+                "type": "image/jpeg"
+            },
+            "alt": {
+                "link": "data/projects/hycon/thumbnail-mobile.webp",
+                "type": "image/webp"
+            },
+            "pc-main": {
+                "link": "data/projects/hycon/thumbnail-pc.jpg",
+                "type": "image/jpeg"
+            },
+            "pc-alt": {
+                "link": "data/projects/hycon/thumbnail-pc.webp",
+                "type": "image/webp"
+            }
+        }
+    },{
+        "name": "cordova mobile apps",
+        "key": "cordova",
+        "skills": "Cordova",
+        "thumbnails": {
+            "main": {
+                "link": "data/projects/cordova-mobile-apps/tracker-mobile.jpg",
+                "type": "image/jpeg"
+            },
+            "alt": {
+                "link": "data/projects/cordova-mobile-apps/tracker-mobile.webp",
+                "type": "image/webp"
+            },
+            "pc-main": {
+                "link": "data/projects/cordova-mobile-apps/tracker-pc.jpg",
+                "type": "image/jpeg"
+            },
+            "pc-alt": {
+                "link": "data/projects/cordova-mobile-apps/tracker-pc.webp",
+                "type": "image/webp"
+            }
+        }
+    },{
+        "name": "block explorer",
+        "key": "block-exporer",
+        "skills": "ReactJS, Blockchain, TypeScript",
+        "thumbnails": {
+            "main": {
+                "link": "data/projects/block-explorer/thumbnail.jpg",
+                "type": "image/jpeg"
+            },
+            "alt": {
+                "link": "data/projects/block-explorer/thumbnail.webp",
+                "type": "image/webp"
+            }
+        }
+    },{
+        "name": "api",
+        "key": "api",
+        "skills": "PHP, BlockChain, Bitcoin, RestAPI",
+        "thumbnails": {
+            "main": {
+                "link": "data/projects/api/success-mobile.jpg",
+                "type": "image/jpeg"
+            },
+            "alt": {
+                "link": "data/projects/api/success-mobile.webp",
+                "type": "image/webp"
+            },
+            "pc-main": {
+                "link": "data/projects/api/success-pc.jpg",
+                "type": "image/jpeg"
+            },
+            "pc-alt": {
+                "link": "data/projects/api/success-pc.webp",
+                "type": "image/webp"
+            }
+        }
+    },{
+        "name": "void detection",
+        "key": "void-detection",
+        "skills": "LabView, MathLab",
+        "thumbnails": {
+            "main": {
+                "link": "data/projects/void-detection/thumbnail-mobile.jpg",
+                "type": "image/jpeg"
+            },
+            "alt": {
+                "link": "data/projects/void-detection/thumbnail-mobile.webp",
+                "type": "image/webp"
+            },
+            "pc-main": {
+                "link": "data/projects/void-detection/thumbnail-pc.jpg",
+                "type": "image/jpeg"
+            },
+            "pc-alt": {
+                "link": "data/projects/void-detection/thumbnail-pc.webp",
+                "type": "image/webp"
+            }
+        }
+    }
+];
 window.addEventListener("DOMContentLoaded", function() {
+    let content="";
+    for (let project of projects) {
+        content+=`
+        <div class="project-content-wrapper item" section="${project.key}">
+            <div class="title">${project.name}</div>
+            <div class="detail">
+                <div>Skills: ${project.skills}</div>
+                ${project.url ? `<div>Url: <a href="${project.url}" target="_blank">${project.url}</a></div>` : ""}
+            </div>
+            <picture>
+                ${project.thumbnails["pc-alt"] ? `<source media="(min-width:600px)" type="${project.thumbnails["pc-alt"].type}" srcset="${project.thumbnails["pc-alt"].link}">` : ""}
+                ${project.thumbnails["pc-main"] ? `<source media="(min-width:600px)" type="${project.thumbnails["pc-main"].type}" srcset="${project.thumbnails["pc-main"].link}">` : ""}
+                <source type="${project.thumbnails.alt.type}" srcset="${project.thumbnails.alt.link}">
+                <img loading="lazy" src="${project.thumbnails.main.link}" alt="${project.name}">
+            </picture>
+            <div class="project-arrow-container"></div>
+        </div>
+        `;
+    }
+    $(".project-wrapper").html(content);
     let mainPos = 0;
     $(".project-wrapper .project-arrow-container, .project-wrapper picture").on("click", function() {                
         let section = $(this).parents(".project-content-wrapper.item").attr("section");
