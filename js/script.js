847fe24d2bc539e353894960e542d1c52d021804
yangnana11 <yphan1988@gmail.com>
2023-08-14 12:44:20 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,39 +1,23 @@
 window.addEventListener("DOMContentLoaded", function() {
-    fetch("https://raw.githubusercontent.com/yangnana11/yangnana11.github.io/master/data/projects/projects.json")
-    .then(r=>r.json())
-    .then(r=>{
-        if (r.length>0) {
-            let ct = "";
-            for(let c of r) {
-                if (c.img["pc-main"]) {
-                    ct+=`<div class="project-content-wrapper">
-                        <div class="detail">
-                            <div class="title">${c.name}</div>
-                            <div>Skills: ${c.skills}</div>
-                        </div>
-                        <picture>
-                            <source media="(min-width:900px)" type="${c.img["pc-alt"].type}" srcset="${c.img["pc-alt"].link}">
-                            <source media="(min-width:900px)" type="${c.img["pc-main"].type}" srcset="${c.img["pc-main"].link}">
-                            <source type="${c.img.alt.type}" srcset="${c.img.alt.link}">
-                            <source type="${c.img.main.type}" srcset="${c.img.main.link}">
-                            <img loading="lazy" src="${c.img.main.link}" alt="${c.name}">
-                        </picture>
-                    </div>`;
-                } else {
-                    ct+=`<div class="project-content-wrapper">
-                        <div class="detail">
-                            <div class="title">${c.name}</div>
-                            <div>Skills: ${c.skills}</div>
-                        </div>
-                        <picture>
-                            <source type="${c.img.alt.type}" srcset="${c.img.alt.link}">
-                            <source type="${c.img.main.type}" srcset="${c.img.main.link}">
-                            <img loading="lazy" src="${c.img.main.link}" alt="${c.name}">
-                        </picture>
-                    </div>`;
-                }
-            }
-            $(".project-wrapper").html(ct);
+    let mainPos = 0;
+    $(".project-wrapper .project-arrow-container, .project-wrapper picture").on("click", function() {                
+        let section = $(this).parents(".project-content-wrapper.item").attr("section");
+        if ($(`.job-content[job-description='${section}']`).length>0) {
+            mainPos = $(window).scrollTop();
+            $("main.container").addClass("hide");
+            $(`.job-content[job-description='${section}']`).addClass("shown");        
+            $([document.documentElement, document.body]).animate({
+                scrollTop: 0
+            }, 0);
+        } else {
+            alert("There is no content of this job");
         }
+    });
+    $(".job-content-close").on("click", function() {
+        $(".job-content").removeClass("shown");
+        $("main.container").removeClass("hide");
+        $([document.documentElement, document.body]).animate({
+            scrollTop: mainPos
+        }, 300);
     })
 });
