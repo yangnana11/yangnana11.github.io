430ae46c28519951d6585600faa55c57155663fd
yangnana11 <yphan1988@gmail.com>
2023-08-14 21:07:19 -0400

    update

--- js/script.js
+++ js/script.js
@@ -21,7 +21,33 @@ const projects = [
                 "link": "data/projects/superhairpieces/thumbnail-pc.webp",
                 "type": "image/webp"
             }
-        }
+        },
+        "description": [
+            "Add and Edit features for Superhairpieces e-commerce, including real-time inventory check, in-stock notification, etc (*). Also I was helping to build landing pages to gain new customers for our business. I also helped in making design of some landing pages.",
+            "(*) We are changing the features design every time so the design may look difference."
+        ],
+        "videos": [
+            {
+                "mobile": "data/projects/superhairpieces/mobile-demo.mov",
+                "pc": "data/projects/superhairpieces/pc-demo.mov"
+            },
+            {
+                "mobile": "data/projects/superhairpieces/mobile-evolve-landing.mov",
+                "pc": "data/projects/superhairpieces/pc-evolve-landing.mov"
+            },
+            {
+                "mobile": "data/projects/superhairpieces/mobile-journey-landing.mov",
+                "pc": "data/projects/superhairpieces/pc-journey-landing.mov"
+            },
+            {
+                "mobile": "data/projects/superhairpieces/inventory-mobile.mov",
+                "pc": "data/projects/superhairpieces/inventory-pc.mov"
+            },
+            {
+                "mobile": "data/projects/superhairpieces/product-mobile.mov",
+                "pc": "data/projects/superhairpieces/product-pc.mov"
+            }
+        ]
     },{
         "name": "privy",
         "key": "privy",
@@ -44,7 +70,16 @@ const projects = [
                 "link": "data/projects/privyusa/thumbnail-pc.webp",
                 "type": "image/webp"
             }
-        }
+        },
+        "description": [
+            "Build server and frontend website for Privy"
+        ],
+        "videos": [
+            {
+                "mobile": "data/projects/privyusa/mobile-demo.mov",
+                "pc": "data/projects/privyusa/pc-demo-final.mov"
+            }
+        ]
     },{
         "name": "chrome extension",
         "key": "chrome",
@@ -66,7 +101,21 @@ const projects = [
                 "link": "data/projects/superhairpieces-extension/thumbnail-pc.webp",
                 "type": "image/webp"
             }
-        }
+        },
+        "description": [
+            "Build a chrome extension for Superhairpieces staff, help them easily for making and manage orders and customers"
+        ],
+        "images": [
+            {
+                "pc": "data/projects/superhairpieces-extension/screenshot1.png"
+            },
+            {
+                "pc": "data/projects/superhairpieces-extension/screenshot2.png"
+            },
+            {
+                "pc": "data/projects/superhairpieces-extension/screenshot3.png"
+            }
+        ]
     },{
         "name": "nowon",
         "key": "nowon",
@@ -89,7 +138,15 @@ const projects = [
                 "link": "data/projects/nowon/thumbnail-pc.webp",
                 "type": "image/webp"
             }
-        }
+        },
+        "description": [
+            "Build a website for Nowon"
+        ],
+        "videos": [
+            {
+                "pc": "data/projects/nowon/video-pc.mov"
+            }
+        ]
     },{
         "name": "next ico",
         "key": "next-ico",
@@ -111,7 +168,18 @@ const projects = [
                 "link": "data/projects/next-ico/thumbnail-pc.webp",
                 "type": "image/webp"
             }
-        }
+        },
+        "description": [
+            "Build a react-native mobile app"
+        ],
+        "videos": [
+            {
+                "mobile": "data/projects/next-ico/video-android-pc.mov",
+            },
+            {
+                "mobile": "data/projects/next-ico/video-ios-pc.mov"
+            }
+        ]
     },{
         "name": "hycon",
         "key": "hycon",
@@ -133,7 +201,15 @@ const projects = [
                 "link": "data/projects/hycon/thumbnail-pc.webp",
                 "type": "image/webp"
             }
-        }
+        },
+        "description": [
+            "Build a website to gather investor for HYCON: a cryptocurrency"
+        ],
+        "videos": [
+            {
+                "pc": "data/projects/hycon/video-pc.mov"
+            }
+        ]
     },{
         "name": "cordova mobile apps",
         "key": "cordova",
@@ -155,7 +231,21 @@ const projects = [
                 "link": "data/projects/cordova-mobile-apps/tracker-pc.webp",
                 "type": "image/webp"
             }
-        }
+        },
+        "description": [
+            "Build mobile apps using Cordova"
+        ],
+        "videos": [
+            {
+                "mobile": "data/projects/cordova-mobile-apps/qr-scanner-pc.MOV",
+            }, 
+            {
+                "mobile": "data/projects/cordova-mobile-apps/qr-scanner-otp.mp4"
+            },
+            {
+                "mobile": "data/projects/cordova-mobile-apps/tracker-pc.MOV"
+            }
+        ]
     },{
         "name": "block explorer",
         "key": "block-exporer",
@@ -169,7 +259,18 @@ const projects = [
                 "link": "data/projects/block-explorer/thumbnail.webp",
                 "type": "image/webp"
             }
-        }
+        },
+        "description": [
+            "Build a webpage for hycon wallet"
+        ],
+        "videos": [
+            {
+                "pc": "data/projects/block-explorer/video-pc.mov",
+            },
+            {
+                "pc": "data/projects/block-explorer/video-mobile.mov"
+            }
+        ]
     },{
         "name": "api",
         "key": "api",
@@ -191,7 +292,18 @@ const projects = [
                 "link": "data/projects/api/success-pc.webp",
                 "type": "image/webp"
             }
-        }
+        },
+        "description": [
+            "Build a back end for Next ICO mobile app"
+        ],
+        "images": [
+            {
+                "mobile": "data/projects/API/error-pc.jpg"
+            },
+            {
+                "mobile": "data/projects/API/success-pc.jpg"
+            }
+        ]
     },{
         "name": "void detection",
         "key": "void-detection",
@@ -213,7 +325,18 @@ const projects = [
                 "link": "data/projects/void-detection/thumbnail-pc.webp",
                 "type": "image/webp"
             }
-        }
+        },
+        "description": [
+            "Build a labview applicaiton to detect void in a program"
+        ],
+        "videos": [
+            {
+                "pc": "data/projects/void-detection/video-pc.mov"
+            },
+            {
+                "pc": "data/projects/void-detection/video-pc.mov"
+            }
+        ]
     }
 ];
 window.addEventListener("DOMContentLoaded", function() {
@@ -240,22 +363,84 @@ window.addEventListener("DOMContentLoaded", function() {
     let mainPos = 0;
     $(".project-wrapper .project-arrow-container, .project-wrapper picture").on("click", function() {                
         let section = $(this).parents(".project-content-wrapper.item").attr("section");
-        if ($(`.job-content[job-description='${section}']`).length>0) {
-            mainPos = $(window).scrollTop();
-            $("main.container").addClass("hide");
-            $(`.job-content[job-description='${section}']`).addClass("shown");        
-            $([document.documentElement, document.body]).animate({
-                scrollTop: 0
-            }, 0);
-        } else {
-            alert("There is no content of this job");
+        let project = projects.find(p=>p.key==section);
+        if (project==undefined) return;
+        $(".job-content").remove();
+        let desp="", imgs="", videos="";
+        for (let d of project.description) {
+            desp+=`<p>${d}</p>`
         }
-    });
-    $(".job-content-close").on("click", function() {
-        $(".job-content").removeClass("shown");
-        $("main.container").removeClass("hide");
+        if (project.images) {
+            for (let i of project.images) {
+                if (i.mobile && i.pc) {
+                    imgs += `
+                    <picture>
+                        <source media="(min-width:600px)" srcset="${i.pc}">
+                        <img loading="lazy" src="${i.mobile}">
+                    </picture>
+                    `;
+                } else {
+                    imgs+=`<img loading="lazy" src="${i.mobile || i.pc}">`;
+                }
+            }
+        }
+        if (project.videos) {
+            for (let v of project.videos) {
+                if (v.pc && v.mobile) {
+                    videos += `
+                    <video width="100%" controls mobile>
+                        <source src="${v.mobile}" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <video width="100%" controls pc>
+                        <source src="${v.pc}" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    `
+                } else if (v.pc) {
+                    videos += `
+                    <video width="100%" controls>
+                        <source src="${v.pc}" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    `;
+                } else {
+                    videos += `
+                    <video class="mobile-only" width="100%" controls>
+                        <source src="${v.mobile}" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    `;
+                }
+            }
+        }
+        $("body").append(`
+        <div class="job-content" job-description="${project.key}">
+            <div class="job-content-menu-container">
+                <div class="job-content-close"></div>
+                <h3>${project.name}</h3>
+            </div>
+            ${desp}
+            <p>Skills: ${project.skills}</p>
+            ${project.url ? `<p>Url: <a href="${project.url}" target="_blank">${project.url}</a></p>` : ""}
+            <div class="job-content-image-wrap">
+                ${imgs}
+                ${videos}
+            </div>
+        </div>
+        `)
+        mainPos = $(window).scrollTop();
+        $("main.container").addClass("hide");
+        $(`.job-content[job-description='${section}']`).addClass("shown");        
         $([document.documentElement, document.body]).animate({
-            scrollTop: mainPos
-        }, 300);
-    })
+            scrollTop: 0
+        }, 0);
+        $(".job-content-close").on("click", function() {
+            $(".job-content").removeClass("shown");
+            $("main.container").removeClass("hide");
+            $([document.documentElement, document.body]).animate({
+                scrollTop: mainPos
+            }, 300);
+        })
+    });
 });
